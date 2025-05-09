116099de63c9620ca036c603202394622fa3b7c6
David Lukens <davidqcy02@gmail.com>
2025-05-09 13:21:04 -0500

    change data frame loading

--- metpy_test.py
+++ metpy_test.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 from metpy.units import units
 
 distance = np.arange(1, 5) * units.meters
@@ -8,4 +9,9 @@ time = units.Quantity(np.arange(2, 10, 2), 'sec')
 
 print(distance / time)
 
-path = "Full-CP20-sounding-dataset/snd-19940409-205903-storm1-10m.zagl"
+path = "Full-CP20-sounding-dataset-clean/snd-19940409-205903-storm1-10m.zagl"
+
+df = pd.read_csv(path)
+df = df.drop(df.columns[0], axis=1)
+
+print(df.head(10))
